472a729b9ac42ba7ed283317a6db758dccd757d2
jkral <jeff@sandboxbrands.com>
2016-07-31 10:11:26 -0400

    assignment 10 commit

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,17 +12,55 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    
+    if ([string1 isEqualToString:string2]) {
+        
+        return YES;
+        
+    } else {
+        
+        return NO;
+    }
+    
+    
+//    return (string1 == string2);
 }
+
+
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    
+    if ([number1 isEqual:number2]) {
+        
+        return YES;
+        
+    } else {
+        
+        return NO;
+    }
+    
+    
+//    return (number1 == number2);
 }
+
+
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    
+    if (integer1 > integer2) {
+        
+        return YES;
+        
+    } else {
+        
+        return NO;
+    }
+    
+    
+//    return NO;
 }
+
 
 @end
