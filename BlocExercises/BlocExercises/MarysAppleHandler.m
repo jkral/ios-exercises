e1bc6ce149d96403887427c7156ae56d6cf547fd
jkral <jeff@sandboxbrands.com>
2016-07-31 11:53:37 -0400

    assignment 11 commit

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,20 +15,58 @@
 
     /* WORK HERE */
     
+    
+    if (dollars == 4) {
+        
+        
+        itemToReturn = @"get out of my store";
+    } else if (dollars == 5){
+        
+        itemToReturn = @"have some gum";
+        
+    } else if (dollars == 6) {
+        
+        itemToReturn = @"have an apple";
+        
+    } else if (dollars == 1000) {
+        
+        itemToReturn = @"have an Apple computer";
+    
+    } else if (dollars == 1000000000) {
+        
+        itemToReturn = @"have The Big Apple";
+        
+    }
+    
+    
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
+
+
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
     /* WORK HERE */
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    
+    cost = self.getsDiscount ? (cost *= .75) : (cost = cost);
+    
+//    if (self.getsDiscount) {
+//        cost *= .75;
+//    }
     
     return cost;
 }
 
+
+
 @end
+
+
+
+
+
+
+
